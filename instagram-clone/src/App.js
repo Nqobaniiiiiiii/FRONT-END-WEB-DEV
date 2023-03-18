016c9bb283b4717a65e2d9f842215289fbadc30e
Nqobani <waynendlovu16@gmail.com>
2023-03-19 00:02:56 +0200

    WEB-DEV

---
+++ instagram-clone/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Post from './Post';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="App_header">
+        <img
+          className ="app__headerImage"
+          src ="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+          alt =""
+        />   
+      </div>
+      <h1>HII my name  instagram</h1>
+      <Post username=""/>
+          
+    </div>
+  )
+}
+
+export default App;
